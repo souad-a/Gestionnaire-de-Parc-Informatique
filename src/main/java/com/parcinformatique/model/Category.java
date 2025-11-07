54c39c9bb6c858d844b9650f54b89020023c6d60
Hafidamesad <hafidamesad.acer@gmail.com>
2025-11-07 21:35:51 +0100

    config: mise à jour hibernate et modèles

--- src/main/java/com/parcinformatique/model/Category.java
+++ src/main/java/com/parcinformatique/model/Category.java
@@ -1,8 +1,6 @@
 package com.parcinformatique.model;
 
-import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "categories")
--- src/main/java/com/parcinformatique/model/Employee.java
+++ src/main/java/com/parcinformatique/model/Employee.java
@@ -1,6 +1,6 @@
 package com.parcinformatique.model;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "employees")
